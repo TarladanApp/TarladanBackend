c582a3558ac5c0c2aa70a767d2262e56ce161cc0
Kaan Hascan <kaanhascann@gmail.com>
2025-05-27 04:34:59 +0300

    performance service için yanlış yapılan instancelar değiştirildi

--- src/performance/performance.service.ts
+++ src/performance/performance.service.ts
@@ -4,12 +4,14 @@ import { Product } from "src/product/entities/product.entity";
 import { Repository } from "typeorm";
 import { PerformanceDto } from "./dto/performance.dto";
 import * as pidusage from 'pidusage';
+import { ProductService } from "src/product/product.service";
 
 @Injectable()
 export class PerformanceService {
     constructor(
         @InjectRepository(Product)
         private readonly productRepository: Repository<Product>,
+        private readonly productService: ProductService,
     ) {}
 
     private calculateMemoryUsage(startMemory: number, endMemory: number): number {
@@ -20,15 +22,16 @@ export class PerformanceService {
         const startTime = Date.now();
         const startMemory = process.memoryUsage().heapUsed;
 
-        const recordCount = await this.productRepository.count();
+        this.productService.findAll();
 
         const endMemory = process.memoryUsage().heapUsed;
         const endTime = Date.now();
 
+        const recordCount = await this.productRepository.count();
         const durationMs = endTime - startTime;
         const memoryUsedKb = this.calculateMemoryUsage(startMemory, endMemory);
 
-        const { cpu} = await pidusage(process.pid);
+        const {cpu} = await pidusage(process.pid);
 
         const dto = new PerformanceDto();
         dto.durationMs = durationMs;
